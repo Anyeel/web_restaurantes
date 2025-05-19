07098d4a79682340ff67d2eb3d60b980e882054f
Anyeel <angelmillanaragon@hotmail.com>
2025-05-19 12:54:29 +0200

    second commit

--- build.js
+++ build.js
@@ -1,27 +1,41 @@
-import {menu} from './menu.js';
-import nunjucks from 'nunjucks';
-import fs from 'fs';
+import { menu } from './menu.js'
+import nunjucks from 'nunjucks'
+import fs from 'fs'
 
-function generateHTML(template, data) {
-    const templateContent = fs.readFileSync(template, 'utf-8');
-    const html = nunjucks.renderString(templateContent, data);
-    return html;
+function generateHTML (template, data) {
+  const templateContent = fs.readFileSync(template, 'utf-8')
+  const html = nunjucks.renderString(templateContent, data)
+  return html
 }
-function createHTMLFile(template, data, outputFile) {
-    const html = generateHTML(template, data);
-    fs.writeFileSync(outputFile, html, 'utf-8');
-    console.log(`Archivo ${outputFile} generado con éxito.`);
+function createHTMLFile (template, data, outputFile) {
+  const html = generateHTML(template, data)
+  fs.writeFileSync(outputFile, html, 'utf-8')
+  console.log(`Archivo ${outputFile} generado con éxito.`)
 }
 
 if (!fs.existsSync('./dist')) {
-    fs.mkdirSync('./dist');
+  fs.mkdirSync('./dist')
 }
-const menuTemplate = './views/menu.njk';
-nunjucks.configure("views",{ autoescape: true });
+const menuTemplate = './views/menu.njk'
+const locationTemplate = './views/location.njk'
+nunjucks.configure('views', { autoescape: true })
 
-createHTMLFile(menuTemplate, { menu }, './dist/index.html');
+createHTMLFile(menuTemplate, { menu }, './dist/index.html')
+createHTMLFile(locationTemplate, {}, './dist/location.html')
 
+const imgsFolder = './imgs'
+const outputImgsFolder = './dist/imgs'
+const imgFiles = fs.readdirSync(imgsFolder)
+if (!fs.existsSync(outputImgsFolder)) {
+  fs.mkdirSync(outputImgsFolder)
+}
+imgFiles.forEach(file => {
+  const srcPath = `${imgsFolder}/${file}`
+  const destPath = `${outputImgsFolder}/${file}`
+  fs.copyFileSync(srcPath, destPath)
+  console.log(`Archivo ${file} copiado a ${outputImgsFolder}.`)
+})
 
-const cssFile = 'style.css';
-const outputCssFile = './dist/style.css';
-fs.copyFileSync(cssFile, outputCssFile);
+const cssFile = 'style.css'
+const outputCssFile = './dist/style.css'
+fs.copyFileSync(cssFile, outputCssFile)
